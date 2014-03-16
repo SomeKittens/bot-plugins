9d4b64d3cb3cb01e24058413abf074b512444080
Amaan Cheval <amaan.1996@gmail.com>
2014-03-17 00:23:16 +0530

    Fixed formatting and added /doge

--- wiki.js
+++ wiki.js
@@ -1,61 +1,61 @@
 module.exports = function (bot, IO) {
-"use strict";
+	"use strict";
 
-function command ( args, cb ) {
-	IO.request({
-		url : 'http://en.wikipedia.org/w/api.php',
-		data : {
-			action : 'opensearch',
-			search : args.toString(),
-			limit : 1,
-			format : 'json'
-		},
-		complete : finish
-	});
+	function command ( args, cb ) {
+		IO.request({
+			url : 'http://en.wikipedia.org/w/api.php',
+			data : {
+				action : 'opensearch',
+				search : args.toString(),
+				limit : 1,
+				format : 'json'
+			},
+			complete : finish
+		});
 
-	function finish ( resp ) {
-		//the result will look like this:
-		// [search_term, [title0, title1, title2, ...]]
-		//we only asked for one result, so the 2nd array will have 1 item
-		var title = resp[ 1 ][ 0 ],
-			base = 'http://en.wikipedia.org/wiki/',
-			found = true, res;
+		function finish ( resp ) {
+			//the result will look like this:
+			// [search_term, [title0, title1, title2, ...]]
+			//we only asked for one result, so the 2nd array will have 1 item
+			var title = resp[ 1 ][ 0 ],
+				base = 'http://en.wikipedia.org/wiki/',
+				found = true, res;
 
-		if ( !title ) {
-			found = false;
-			res = [
-				'No result found',
-				'The Wikipedia contains no knowledge of such a thing',
-				'The Gods of Wikipedia did not bless us'
-			].random();
-		}
-		else {
-			//for some reason, wikipedia can't simply return a url
-			title = encodeURIComponent( title.replace(/ /g, '_') );
+			if ( !title ) {
+				found = false;
+				res = [
+					'No result found',
+					'The Wikipedia contains no knowledge of such a thing',
+					'The Gods of Wikipedia did not bless us'
+				].random();
+			}
+			else {
+				//for some reason, wikipedia can't simply return a url
+				title = encodeURIComponent( title.replace(/ /g, '_') );
 
-			res = base + title;
-		}
+				res = base + title;
+			}
 
-		if ( cb && cb.call ) {
-			cb( res );
-		}
-		else if ( found ){
-			args.directreply( res );
-		}
-		else {
-			args.reply( res );
+			if ( cb && cb.call ) {
+				cb( res );
+			}
+			else if ( found ){
+				args.directreply( res );
+			}
+			else {
+				args.reply( res );
+			}
 		}
 	}
-}
 
-bot.addCommand({
-	name : 'wiki',
-	fun : command,
-	permissions : {
-		del : 'NONE'
-	},
+	bot.addCommand({
+		name : 'wiki',
+		fun : command,
+		permissions : {
+			del : 'NONE'
+		},
 
-	description : 'Search Wikipedia. `/wiki term`',
-	async : true
+		description : 'Search Wikipedia. `/wiki term`',
+		async : true
 });
 };
