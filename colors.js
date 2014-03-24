f7c9a0dd4f029d5f7031ac2c0e37196a7ca0a9f0
Jhawins <j.hawkinss@yahoo.com>
2014-03-24 11:22:08 -0400

    Wrapped in module goop

--- colors.js
+++ colors.js
@@ -1,27 +1,27 @@
 'use strict';
-
-function color ( args ) {
-	var base = 'http://somethinghitme.com/color/' +
-		'colors.php?color=';
-	var param = args.toString()
-		.toLowerCase()
-		.match( /([a-z0-9]+)+/g )
-		.join( ',' );
-
-
-	args.directreply( base + param + '#.png' );
-}
-
-module.exports = {
-	name : 'color',
-	fun : color,
-	permissions : {
-		del : 'NONE'
-	},
-
-	description : 'Displays the' +
-		'color(s) passed in as RGB, hexadecimal' +
-		'or hex-shorthand or standard color names' +
-		'(if it works in CSS it should work here). ' +
-		' `/color color0[ color1[ ...]]`'
+module.exports = function(bot, IO) {
+	function color ( args ) {
+		var base = 'http://somethinghitme.com/color/' +
+			'colors.php?color=';
+		var param = args.toString()
+			.toLowerCase()
+			.match( /([a-z0-9]+)+/g )
+			.join( ',' );
+	
+	
+		args.directreply( base + param + '#.png' );
+	}
+	
+	bot.addCommand({
+		name : 'color',
+		permissions : {
+			del : 'NONE'
+		},
+	
+		description : 'Displays the' +
+			'color(s) passed in as RGB, hexadecimal' +
+			'or hex-shorthand or standard color names' +
+			'(if it works in CSS it should work here). ' +
+			' `/color color0[ color1[ ...]]`'
+	};
 };
